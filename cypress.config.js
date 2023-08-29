e27a9f8946dfceea95906bd1a64935c220ccd2f3
K4iyzen <driss.khettab@gmail.com>
2023-08-29 15:58:05 +0200

    cypress 05

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,43 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('<event>', (arg1, arg2) => {
+      })
     },
   },
-  projectId: 'k61z49',
+  reporter: "nyan", 
+})
+
+const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("task", { downloadFile });
+    },
+    projectId: 'k61z49',
+  },
+});
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    projectId: 'k61z49',
+  },
+});
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+    projectId: 'k61z49',
+
+  },
 });
